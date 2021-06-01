84d085059b9e7a59d55e04634c0b94a4e17c9b06
Jason Lord <17jlord.stem@gmail.com>
2021-05-31 23:39:32 -0400

    Creating Twitter Sentiment Analysis File
    Just made the file and linked video following will do tomorrow probably

---
+++ DatascienceGithub/TwitterSentimentAnalysis.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 23:37:27 2021
+https://www.youtube.com/watch?v=o_OZdbCzHUA
+Creating a program to analyze twitter sentiments quickly 
+
+@author: Jason @jpl922
+"""
+
