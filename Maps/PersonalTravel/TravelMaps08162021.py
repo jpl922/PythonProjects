27ba2d60e70a7bbc3209376c9044ba47c2373bf0
Jason Lord <17jlord.stem@gmail.com>
2021-08-16 18:24:21 -0400

    Personal Map project 1

---
+++ Maps/PersonalTravel/TravelMaps08162021.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 18:22:18 2021
+Setting up a personal travel map up to 08162021 for vacations up to this point 
+Will not see much until after csv setup
+@author: @jpl922
+"""
+
