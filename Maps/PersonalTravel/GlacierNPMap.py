9b0e66d0921b530e2e7f48756346e7a49275cb1c
Jason Lord <17jlord.stem@gmail.com>
2021-08-16 20:03:32 -0400

    GlacierNP Project

---
+++ Maps/PersonalTravel/GlacierNPMap.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 18:22:18 2021
+Setup a glacier map actually seems more interesting (trailheads etc..)
+
+Plan 
+Glacier national park; therefore montana map 
+Try to do something with trail data 
+Datas 
+@author: @jpl922
+"""
+
--- Maps/PersonalTravel/TravelMaps08162021.py
+++ Maps/PersonalTravel/TravelMaps08162021.py
@@ -3,6 +3,11 @@
 Created on Mon Aug 16 18:22:18 2021
 Setting up a personal travel map up to 08162021 for vacations up to this point 
 Will not see much until after csv setup
+
+
+Plans or desired features that might be cool 
+Location, year traveled, something else 
+calculate age to determine age of visits and see gaps. 
 @author: @jpl922
 """
 
